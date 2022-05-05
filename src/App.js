083421a0ca232e3f9e12f1f7bcd83ce527015fb2
Sanjid <sanjidk24@gmail.com>
2022-05-05 17:24:20 +0600

    Header with route Done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,37 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Header from './components/Shared/Header/Header';
+import Footer from './components/Shared/Footer/Footer';
+import NotFound from './components/Shared/NotFound/NotFound';
+
+import Login from './components/Login/Login/Login';
+import Signup from './components/Login/Signup/Signup';
+
+import AddItem from './components/Main/AddItem/AddItem';
+import Blogs from './components/Main/Blogs/Blogs';
+import Home from './components/Main/Home/Home';
+import MyItems from './components/Main/MyItems/MyItems';
+import Items from './components/Main/Items/Items';
+import ManageItems from './components/Main/ManageItems/ManageItems';
 
 function App() {
   return (
-    <div>
+    <div className='App'>
       <Header></Header>
-     <h1>Hello from App JS Eid Mubarak</h1>
+      <Routes>
+      <Route path="/" element={<Home></Home>}></Route>
+        <Route path="/home" element={<Home></Home>}></Route>
+        <Route path="/blogs" element={<Blogs></Blogs>} ></Route>
+        <Route path="/items" element={<Items></Items>} ></Route>
+        <Route path="/additem" element={<AddItem></AddItem>} ></Route>
+        <Route path="/myitems" element={<MyItems></MyItems>} ></Route>
+        <Route path="/manageitems" element={<ManageItems></ManageItems>} ></Route>
+
+        <Route path="/login" element={<Login></Login>} ></Route>
+        <Route path="/signup" element={<Signup></Signup>} ></Route>
+        <Route path="*" element={<NotFound></NotFound>} ></Route>
+      </Routes>
+      <Footer></Footer>
     </div>
   );
 }
--- src/components/Shared/Header/Header.js
+++ src/components/Shared/Header/Header.js
@@ -1,11 +1,32 @@
 import React from 'react';
+import { Container, Nav, Navbar } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import './Header.css';
 
 const Header = () => {
   return (
-    <div>
-            <h1>My name is Header</h1>
+    <Navbar collapseOnSelect expand="lg" sticky='top' bg="dark" variant="dark">
+    <Container>
+      <Navbar.Brand className='fw-bold' as={Link} to="/"><span className='header-title'>ComputeR</span> <span className='header-title2'>WareHousE</span>
+      </Navbar.Brand>
+      <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+      <Navbar.Collapse id="responsive-navbar-nav">
+        <Nav className="me-auto text-center fw-bold">
+          <Nav.Link className='text-white header-compo' as={Link} to="/home">Home</Nav.Link>
+          <Nav.Link className='text-white header-compo' as={Link} to="/items">Items</Nav.Link>
+          <Nav.Link className='text-white header-compo' as={Link} to="/manageitems">Manage Items</Nav.Link>
+          <Nav.Link className='text-white header-compo' as={Link} to="/additem">Add Item</Nav.Link>
+          <Nav.Link className='text-white header-compo' as={Link} to="/myitems">My Items</Nav.Link>
+          <Nav.Link className='text-white header-compo' as={Link} to="/blogs">Blogs</Nav.Link>
+        </Nav>
 
-    </div>
+        <Nav className="text-center fw-bold">
+        <Nav.Link className='text-white header-compo' as={Link} to="/login">Login</Nav.Link>
+        <Nav.Link className='text-white header-compo' as={Link} to="/signup">Sign Up</Nav.Link>
+        </Nav>
+      </Navbar.Collapse>
+    </Container>
+  </Navbar>
   );
 };
 
