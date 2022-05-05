ae3c9d349b49f45e481fea475c8656ecb93b241c
Sanjid <sanjidk24@gmail.com>
2022-05-05 17:35:14 +0600

    simple footer done

--- src/components/Shared/Footer/Footer.js
+++ src/components/Shared/Footer/Footer.js
@@ -1,9 +1,12 @@
 import React from 'react';
 
 const Footer = () => {
+  const today = new Date();
+  const year = today.getFullYear();
+
   return (
-    <div>
-      <h1>My name is Footer</h1>
+    <div className='bg-dark text-white footer-div py-4'>
+      <p className='m-0'>Copyright &#169; {year} Computer Warehouse by Mahmud Sanjid</p>
     </div>
   );
 };
