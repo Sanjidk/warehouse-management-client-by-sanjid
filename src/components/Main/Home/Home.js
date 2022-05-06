07734a99d527bff93d90e31d91bcb2c5c3b9c9cc
Sanjid <sanjidk24@gmail.com>
2022-05-06 18:43:04 +0600

    Extra part upcoming

--- src/components/Main/Home/Home.js
+++ src/components/Main/Home/Home.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import BannerHome from '../BannerHome/BannerHome';
+import UpcomingStock from '../UpcomingStock/UpcomingStock';
 
 const Home = () => {
   return (
     <div>
       <BannerHome></BannerHome>
+      
+      <UpcomingStock></UpcomingStock>
 
     </div>
   );
---
+++ src/components/Main/UpcomingStock/UpcomingStock.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import apple1 from "../../../img/item/apple1.png";
+
+
+const UpcomingStock = () => {
+  return (
+    <div>
+      <h1 className="mt-5 mb-0 p-2 text-white bg-info fw-bold">Upcoming Items</h1>
+      <div className="w-100 border-0">
+        <div className="row g-0">
+          <div className="col-md-4 p-3 shadow-lg">
+            <img src={apple1} className="img-fluid rounded-start" alt="..." />
+          </div>
+          <div className="col-md-8 shadow-lg">
+            <div className="card-body">
+              <h2 className="card-title">Water Color Apple</h2>
+              <h5>Quantity: Not Available Now </h5>
+              <p className="card-text text-start w-75 mx-auto">
+              The MacRumors guide to upcoming products is designed to provide an overview of everything that we're expecting to see from Apple, both in the near and more distant future. Dates listed are not always concrete, but are based on the most recent rumors and information that we have.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default UpcomingStock;
