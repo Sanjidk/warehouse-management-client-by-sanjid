75c32866749cbf7c71fd9ece7f9cca8cb669f815
Sanjid <sanjidk24@gmail.com>
2022-05-08 03:17:44 +0600

    not found page

--- src/components/Shared/NotFound/NotFound.js
+++ src/components/Shared/NotFound/NotFound.js
@@ -1,9 +1,10 @@
-import React from 'react';
+import React from "react";
+import notFound from '../../../img/notFound.jpg'
 
 const NotFound = () => {
   return (
-    <div>
-      <h1>My name is Not Found</h1>
+    <div >
+      <img height={546} className="w-100" src={notFound} alt="" />{" "}
     </div>
   );
 };
