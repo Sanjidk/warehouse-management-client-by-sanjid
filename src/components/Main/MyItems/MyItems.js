66e64269d2a8329ead42e4d0602f2c41bdfaf0c7
Sanjid <sanjidk24@gmail.com>
2022-05-09 16:55:41 +0600

    myitems jwt token

--- src/components/Main/MyItems/MyItems.js
+++ src/components/Main/MyItems/MyItems.js
@@ -1,24 +1,41 @@
+import axios from 'axios';
+import { signOut } from 'firebase/auth';
 import React from 'react';
 import { useEffect, useState } from 'react';
 import { Spinner } from 'react-bootstrap';
 import { useAuthState } from 'react-firebase-hooks/auth';
+import { useNavigate } from 'react-router-dom';
 import auth from '../../../firebase.init';
 
 
 const MyItems = () => {
   const [user, loading] = useAuthState(auth);
-  const [myitems, setMyitems] = useState([]);
+  const [items, setItems] = useState([]);
+  const navigate = useNavigate();
 
-  useEffect(()=>{
-    const newItems = async () =>{
-      const email = user.email;
-      const url = `http://localhost:5000/myitems?email=${email}`;
-      await fetch(url)
-      .then(res=> res.json())
-      .then(data => setMyitems(data))
-    }
-    newItems();
-  },[user])
+  
+        useEffect(()=>{   
+          const getMyItems = async () => { 
+            const email = user.email;     
+            const url = `http://localhost:5000/myitems?email=${email}`;
+              try{
+                  const {data} = await axios.get(url ,{
+                      headers: {
+                          authorization: `Bearer ${localStorage.getItem("accessToken")}`,
+                      }
+                  });
+                  setItems(data);
+                }
+                  catch (error){
+                      console.log(error.message);
+                  if (error.response.status === 401 || error.response.status === 403) {
+                  signOut(auth);
+                  navigate("/login");
+                  }
+              }
+          }
+              getMyItems();
+      },[user])
 
   if (loading) {
     return <Spinner animation="grow" variant="dark" />;
@@ -35,15 +52,15 @@ const MyItems = () => {
           .then(res => res.json())
           .then(data => {
               console.log(data);
-              const remaining = myitems.filter(item => item._id !== id);
-              setMyitems(remaining);
+              const remaining = items.filter(item => item._id !== id);
+              setItems(remaining);
           })
       }
   }
 
   return (
     <div className='container'>
-            <h1 className='text-center text-white bg-info w-50 mx-auto p-2 m-2 rounded'>My Items : {myitems.length}</h1>
+            <h1 className='text-center text-white bg-info w-50 mx-auto p-2 m-2 rounded'>My Items : {items.length}</h1>
             <table className="table border border-dark border-3">
                 <thead>
                     <tr>
@@ -56,7 +73,7 @@ const MyItems = () => {
                     </tr>
                 </thead>
                 {
-                    myitems.map(item =>
+                    items.map(item =>
                
                 <tbody >
                 
