10f301dc0fe47551cb40c03e0ec6f28a3a5c7758
Mahmud Sanjid <sanjidk24@gmail.com>
2022-05-30 17:44:02 +0600

    solve heroku3

--- src/components/Hooks/useItems.js
+++ src/components/Hooks/useItems.js
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 
 const useItems = () => {
   const [items, setItems] = useState([]);
+  
 
   useEffect(() => {
     fetch("https://murmuring-escarpment-19187.herokuapp.com/inventory")
